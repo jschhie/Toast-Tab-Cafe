afcb0898df79b58d966d093798786d75f365ade3
Jennifer Chhie <jschhie@ucdavis.edu>
2024-08-16 12:31:53 -0700

    updated models

--- website/models.py
+++ website/models.py
@@ -6,8 +6,8 @@ class Drink(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
     price = db.Column(db.String(20))
-    img_url = db.Column(db.String(40), unique=True)
-    thumbnail_url = db.Column(db.String(40), unique=True)
+    img_url = db.Column(db.String(40))
+    thumbnail_url = db.Column(db.String(40))
     desc = db.Column(db.String(80))
     tag = db.Column(db.String(20))
 
